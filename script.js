f019bd4168d7f4698b91ea8d3ee9e5a3da4d74ba
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2024-03-15 23:35:18 +0700

    Change slider range to select option input
    Select option input more convenient than slider for choosing building level

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const select = document.querySelector("#buildings")
-const levelSlider = document.querySelector('#levelSlider');
-const sliderValue = document.querySelector('#sliderValue')
+const selectBuildingType = document.querySelector("#buildingType")
+const selectBuildingLevel = document.querySelector('#buildingLevel')
 
 const buildingTypes = ["Aerospace electronics", "Aerospace factory", "Automotive R&D", "Bakery", "Bank", "Beverage factory", "Breeding laboratory"]
 
@@ -8,11 +7,12 @@ buildingTypes.forEach(value => {
     let option = document.createElement("option");
     option.text = value;
     option.value = value;
-    select.appendChild(option);
+    selectBuildingType.appendChild(option);
 })
 
-sliderValue.innerText = levelSlider.value;
-
-levelSlider.addEventListener("input", () => {
-    sliderValue.innerText = levelSlider.value
-})
+for (let i = 1; i <= 50; i++){
+    let option = document.createElement("option");
+    option.text = i;
+    option.value = i;
+    selectBuildingLevel.appendChild(option)
+}
